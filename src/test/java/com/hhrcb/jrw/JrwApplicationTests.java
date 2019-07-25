b5425aca643cb35fce97c951fe938c4f9b31de03
QHDLOVEMMN <qhd123698745@163.com>
2019-07-25 14:45:07 +0800

    init repo

---
+++ src/test/java/com/hhrcb/jrw/JrwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hhrcb.jrw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JrwApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
